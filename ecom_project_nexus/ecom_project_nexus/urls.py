455fb76fac972ae76bc61919b8463d69af4b8316
sofi <bedlusofi@gmail.com>
2025-03-01 16:51:26 +0300

    refactor: update authentication URL and include DRF authentication URLs

--- ecom_project_nexus/ecom_project_nexus/urls.py
+++ ecom_project_nexus/ecom_project_nexus/urls.py
@@ -50,8 +50,9 @@ Query parameters enable filtering, sorting, and pagination.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('accounts.urls')),
+    path('auth/', include('accounts.urls')),
     path('api/', include('products.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
