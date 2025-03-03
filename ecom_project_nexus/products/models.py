55b933d6a48cd03d6f704a5700d1fffc631e9fd4
sofi <bedlusofi@gmail.com>
2025-03-03 12:09:51 +0300

    feat: enforce unique category names and add validation in serializer

--- ecom_project_nexus/products/models.py
+++ ecom_project_nexus/products/models.py
@@ -3,7 +3,7 @@ import uuid
 
 class Category(models.Model):
     catagory_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, unique=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- ecom_project_nexus/products/serializers.py
+++ ecom_project_nexus/products/serializers.py
@@ -6,6 +6,11 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['catagory_id', 'name', 'description', 'created_at', 'updated_at']
 
+    def validate_name(self, value):
+        if Category.objects.filter(name__iexact=value).exists():
+            raise serializers.ValidationError("A category with this name already exists.")
+        return value
+
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), source='category', write_only=True)
